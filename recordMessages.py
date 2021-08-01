b46df12eff0bebec34fdcea5c734276972598c3d
Leucocyte123 <594425947@qq.com>
2021-08-01 11:30:49 -0400

    Update to MySQL version

--- recordMessages.py
+++ recordMessages.py
@@ -1,49 +1,60 @@
 # -*- coding: utf-8 -*-
-import os
-import sys
-from datetime import datetime
+
+import getpass
+
 import asyncio
+import mysql.connector
 
 from blivedm.blivedm import BLiveClient, DanmakuMessage
-from config import all_room_ids
 
-filename = 'log/undefined.txt'
-def setFilename(name):
-    global filename
-    filename = 'log/danmu-%s-%s.txt' % (name, datetime.now().strftime('%Y-%m-%d-%H-%M-%S'))
-def getFilename():
-    global filename
-    return filename
+VtuberOfInterest = {
+    'xuehusang': 24393,
+    'kitzuki': 22889484,
+}
+
+selected_vtuber = 'xuehusang'
+room_id = VtuberOfInterest[selected_vtuber]
+
+mysqlHost = '172.17.0.4'
+mysqlUsername = 'root'
+mysqlPassword = ''
+
+def inputMysqlPassword():
+    global mysqlPassword
+    mysqlPassword = getpass.getpass(prompt='MySQL Password: ')
+
+def parseMessage(message: DanmakuMessage):
+    uname = message.username
+    uid = message.uid
+    danmu = message.msg
+    return uname, uid, danmu
+
+def writeMySQL(val):
+    mydb = mysql.connector.connect(
+        host=mysqlHost,
+        user=mysqlUsername,
+        password=mysqlPassword,
+        database=selected_vtuber
+    )
+    mycursor = mydb.cursor()
+
+    sql = "INSERT INTO messages (uname, uid, message) VALUES (%s, %s, %s)"
+
+    mycursor.execute(sql, val)
+    mydb.commit()
 
 class MyBLiveClient(BLiveClient):
-    async def _on_receive_danmaku(self, danmaku: DanmakuMessage):
-        print(f'{danmaku.uname}：{danmaku.msg}')
-        with open(getFilename(), 'a', encoding='utf-8') as f:
-            f.write(danmaku.msg)
-            f.write('\n')
+    async def _on_receive_danmaku(self, message: DanmakuMessage):
+        val = parseMessage(message)
+        writeMySQL(val)
 
 async def main():
-    if len(sys.argv) > 1:
-        room_name = sys.argv[1]
-    else:
-        room_name = '雪狐桑'
-    room_id = all_room_ids[room_name]
-    setFilename(room_name)
-
-    # 如果SSL验证失败就把ssl设为False
     client = MyBLiveClient(room_id, ssl=True)
     future = client.start()
     try:
-        # 5秒后停止，测试用
-        # await asyncio.sleep(5)
-        # future = client.stop()
-        # 或者
-        # future.cancel()
-
         await future
     finally:
         await client.close()
-
 
 if __name__ == '__main__':
     asyncio.get_event_loop().run_until_complete(main())
