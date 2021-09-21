7218d0ad9f359f5bf13e69077cc6d9906f2f1a98
Leucocyte123 <594425947@qq.com>
2021-09-21 09:49:09 -0400

    Update

--- generateWordCloud.py
+++ generateWordCloud.py
@@ -9,12 +9,23 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 
-ignored_words = {'你', '我', '的', '了', '是', '啊', '吧', '不', '这', '吗', '有', '还', '也', '没', '就', '都', '晚安', '晚上好', '哈哈哈', '哈哈哈哈', '好耶'}
+ignored_words = {
+    '你', '我', '的', '了', '是', '啊', '吧', '不', '这', '吗', '有', '还', '也', '没', '就', '都', '好', '把', '被', '耶',
+    '晚安', '好耶', '晚上',
+    '晚上好',
+    # '哈哈', '哈哈哈', '哈哈哈哈',
+}
 # ignored_words = {}
-added_words = {'内鬼', '雪狐', '晚上好', '好耶'}
+added_words = {
+    '内鬼', 
+    '雪狐', 
+    '我不能接受',
+    '好人', '坏人', '好女人', '坏女人'
+}
+# added_words = {}
 
 def filterChinese(line):
-    regStr = ".*?([\u4E00-\u9FA5]+).*?"
+    regStr = ".*?([\?\u4E00-\u9FA5]+).*?"
     ret = re.findall(regStr, line)
     if ret:
         return ret
@@ -25,9 +36,9 @@ def main():
     filename = sys.argv[1]
 
     # Import background image
-    shape_image = Image.open("materials/初号狐.png")
+    # shape_image = Image.open("materials/初号狐.png")
     # shape_image = shape_image.resize((int(shape_image.width * 2), int(shape_image.height * 2)))
-    shape_image = np.array(shape_image)
+    # shape_image = np.array(shape_image)
 
     # Read messages
     with open(filename, encoding='utf-8') as f:
@@ -52,33 +63,33 @@ def main():
     # Generate word cloud
     font = r'C:\Windows\Fonts\MSYHL.TTC'
     
-    # wc = wordcloud.WordCloud(background_color="white", font_path=font, width=800, height=400)
+    wc = wordcloud.WordCloud(background_color="white", font_path=font, width=800, height=400)
     # wc = wordcloud.WordCloud(background_color='#7FFFD4', font_path=font, mask=shape_image)
-    wc = wordcloud.WordCloud(mode='RGBA', background_color='rgba(255, 255, 255, 0)', font_path=font, mask=shape_image)
+    # wc = wordcloud.WordCloud(mode='RGBA', background_color='rgba(255, 255, 255, 0)', font_path=font, mask=shape_image)
     wc.generate_from_frequencies(frequency)
     
     # Set color from image
-    img_colors = wordcloud.ImageColorGenerator(shape_image)
-    wc.recolor(color_func=img_colors)
+    # img_colors = wordcloud.ImageColorGenerator(shape_image)
+    # wc.recolor(color_func=img_colors)
 
     # Add background
-    image = Image.fromarray(wc.to_array())
-    background = Image.open('materials/钢板.png').convert("RGBA")
-    print (image.size, background.size)
-    image = Image.alpha_composite(background, image)
+    # image = Image.fromarray(wc.to_array())
+    # background = Image.open('materials/钢板.png').convert("RGBA")
+    # print (image.size, background.size)
+    # image = Image.alpha_composite(background, image)
     
     # Save to file
-    # wc.to_file('%s.png' % filename)
-    plt.figure()
-    plt.axis('off')
-    fig = plt.imshow(image, interpolation='nearest')
-    fig.axes.get_xaxis().set_visible(False)
-    fig.axes.get_yaxis().set_visible(False)
-    plt.savefig('log/test.png',
-                bbox_inches='tight',
-                pad_inches=0,
-                format='png',
-                dpi=300)
+    wc.to_file('%s.png' % filename)
+    # plt.figure()
+    # plt.axis('off')
+    # fig = plt.imshow(image, interpolation='nearest')
+    # fig.axes.get_xaxis().set_visible(False)
+    # fig.axes.get_yaxis().set_visible(False)
+    # plt.savefig('log/test.png',
+    #             bbox_inches='tight',
+    #             pad_inches=0,
+    #             format='png',
+    #             dpi=300)
 
 
 if __name__ == '__main__':
